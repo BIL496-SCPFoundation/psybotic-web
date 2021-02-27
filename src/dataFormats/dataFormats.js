45dd6ad634e243490637a5a71874f3748610810d
Lumpus99 <cakiroglu.mert@gmail.com>
2021-02-27 21:14:02 +0300

    Added dataFormats.js file to support different tables

---
+++ src/dataFormats/dataFormats.js
@@ -0,0 +1,28 @@
+export const FAMILY_MEMBER = [
+    {
+        Header: 'Name',
+        columns: [
+            {
+                Header: 'First Name',
+                accessor: 'firstName',
+            },
+            {
+                Header: 'Last Name',
+                accessor: 'lastName',
+            },
+        ],
+    },
+    {
+        Header: 'Contact',
+        columns: [
+            {
+                Header: 'Phone',
+                accessor: 'phone',
+            },
+            {
+                Header: 'Mail',
+                accessor: 'mail',
+            },
+        ],
+    },
+]
