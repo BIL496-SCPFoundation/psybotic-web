cc3bb34a269b41eeeaa49e3d1ab45935b62a03ff
Lumpus99 <cakiroglu.mert@gmail.com>
2021-03-23 11:43:08 +0300

    Added filtering and sorting of the data

--- src/components/elements/Grid.js
+++ src/components/elements/Grid.js
@@ -7,6 +7,8 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 import '../../assets/css/agGrid.css'
 import {USER} from '../../utils/data/DataFormats'
 import UserService from "../../utils/data/axios/services/UserService";
+import ButtonGroup from "./ButtonGroup";
+import Button from "./Button";
 
 const Grid = () => {
     const [gridApi, setGridApi] = useState(null);
@@ -18,21 +20,23 @@ const Grid = () => {
         setGridColumnApi(params.columnApi);
 
         const updateData = (data) => {
-            data.forEach(function (data, index) {
-                data.id = 'R' + (index + 1);
-            });
-            var dataSource = {
+            let dataSource = {
                 rowCount: null,
                 getRows: function (params) {
-                    console.log('asking for ', params);
+                    console.log('asking for ' + params.startRow + ' to ' + params.endRow);
                     setTimeout(function () {
-                        var rowsThisPage = data.slice(
+                        const dataAfterSortingAndFiltering = sortAndFilter(
+                            data,
+                            params.sortModel,
+                            params.filterModel
+                        );
+                        const rowsThisPage = dataAfterSortingAndFiltering.slice(
                             params.startRow,
                             params.endRow
                         );
-                        var lastRow = -1;
-                        if (data.length <= params.endRow) {
-                            lastRow = data.length;
+                        let lastRow = -1;
+                        if (dataAfterSortingAndFiltering.length <= params.endRow) {
+                            lastRow = dataAfterSortingAndFiltering.length;
                         }
                         params.successCallback(rowsThisPage, lastRow);
                     }, 500);
@@ -40,65 +44,135 @@ const Grid = () => {
             };
             params.api.setDatasource(dataSource);
         };
+
         userService.findByPagination(1000).then((data) => updateData(data.data._embedded.users));
-        /*
-        fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
-            .then((resp) => resp.json())
-            .then((data) => updateData(data));*/
+
+    };
+
+
+    const onSelectionChanged = () => {
+        let selectedRows = gridApi.getSelectedRows();
+        console.log(selectedRows);
     };
 
     return (
-        <div style={{width: '1140px', height: '700px'}}>
-            <div
-                id="myGrid"
-                style={{
-                    height: '100%',
-                    width: '100%',
-                }}
-                className="ag-theme-alpine-dark"
-            >
-                <AgGridReact
-                    defaultColDef={{
-                        flex: 1,
-                        minWidth: 150,
-                        sortable: true,
-                        resizable: true,
-                        floatingFilter: true,
+        <div>
+            <div style={{width: '1140px', height: '700px'}}>
+                <div
+                    id="myGrid"
+                    style={{
+                        height: '100%',
+                        width: '100%',
                     }}
-                    components={{
-                        loadingRenderer: function (params) {
-                            if (params.value !== undefined) {
-                                return params.value;
-                            } else {
-                                return '<img src="https://www.ag-grid.com/example-assets/loading.gif">';
-                            }
-                        },
-                    }}
-                    rowSelection={'multiple'}
-                    rowModelType={'infinite'}
-                    cacheOverflowSize={2}
-                    maxConcurrentDatasourceRequests={2}
-                    infiniteInitialRowCount={1}
-                    maxBlocksInCache={2}
-                    pagination={true}
-                    paginationPageSize={10}
-                    cacheBlockSize={10}
-                    getRowNodeId={function (item) {
-                        return item.id;
-                    }}
-                    onGridReady={onGridReady}
+                    className="ag-theme-alpine-dark"
                 >
-                    <AgGridColumn checkboxSelection={true} maxWidth={40} sortable={false}/>
-                    {USER.map(((value) => {
-                        return (<AgGridColumn headerName={value.Header} field={value.accessor} sortable={false}/>);
-                    }))}
+                    <AgGridReact
+                        defaultColDef={{
+                            flex: 1,
+                            minWidth: 150,
+                            sortable: true,
+                            resizable: true,
+                            floatingFilter: true,
+                        }}
+                        components={{
+                            loadingRenderer: function (params) {
+                                if (params.value !== undefined) {
+                                    return params.value;
+                                } else {
+                                    return '<img src="https://www.ag-grid.com/example-assets/loading.gif" alt="Loading...">';
+                                }
+                            },
+                        }}
+                        rowSelection={'multiple'}
+                        rowModelType={'infinite'}
+                        onSelectionChanged={onSelectionChanged}
+                        cacheOverflowSize={2}
+                        maxConcurrentDatasourceRequests={2}
+                        infiniteInitialRowCount={1}
+                        maxBlocksInCache={2}
+                        pagination={true}
+                        paginationPageSize={10}
+                        cacheBlockSize={10}
+                        getRowNodeId={function (item) {
+                            return item.id;
+                        }}
+                        onGridReady={onGridReady}
+                    >
+                        <AgGridColumn checkboxSelection={true} maxWidth={40} sortable={false}/>
+                        {USER.map(((value, index) => {
+                            return (<AgGridColumn key={index} filter="agTextColumnFilter"
+                                                  filterParams={{
+                                                      filterOptions: ['includes', 'equals'],
+                                                  }} headerName={value.Header} field={value.accessor}
+                                                  sortable={true}/>);
+                        }))}
 
 
-                </AgGridReact>
+                    </AgGridReact>
+                </div>
             </div>
+            <ButtonGroup style={{"paddingTop": "30px"}}>
+                <Button type="button" className="button-secondary">Edit</Button>
+                <Button type="button" className="button-primary">Delete</Button>
+                <Button type="button" className="button-dark">Return</Button>
+            </ButtonGroup>
         </div>
     );
 };
 
-// cellRenderer="loadingRenderer"
+function sortAndFilter(allOfTheData, sortModel, filterModel) {
+    return sortData(sortModel, filterData(filterModel, allOfTheData));
+}
+
+function sortData(sortModel, data) {
+    let sortPresent = sortModel && sortModel.length > 0;
+    if (!sortPresent) {
+        return data;
+    }
+    let resultOfSort = data.slice();
+    resultOfSort.sort(function (a, b) {
+        for (let k = 0; k < sortModel.length; k++) {
+            const sortColModel = sortModel[k];
+            const valueA = a[sortColModel.colId];
+            const valueB = b[sortColModel.colId];
+            if (valueA === valueB) {
+                continue;
+            }
+            let sortDirection = sortColModel.sort === 'asc' ? 1 : -1;
+            if (valueA > valueB) {
+                return sortDirection;
+            } else {
+                return sortDirection * -1;
+            }
+        }
+        return 0;
+    });
+    return resultOfSort;
+}
+
+function filterData(filterModel, data) {
+    let filterPresent = filterModel && Object.keys(filterModel).length > 0;
+    if (!filterPresent) {
+        return data;
+    }
+    let resultOfFilter = [];
+
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        for (let prop in filterModel) {
+            if (Object.prototype.hasOwnProperty.call(filterModel, prop)) {
+                let filterField = filterModel[prop];
+                let value = item[prop].toLowerCase();
+                let filterValue = filterField.filter.toLowerCase()
+                if (filterField.type === "equals" && filterValue !== value)
+                    continue;
+                else if(filterField.type === "includes" && !value.includes(filterValue))
+                    continue;
+            }
+            resultOfFilter.push(item);
+        }
+    }
+    return resultOfFilter;
+}
+
 export default Grid;
